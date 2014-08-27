0a23df545a89307ec90c9c64d788fce222544667
hasitha <hasithadkr7@gmail.com>
2014-08-27 13:06:51 +0530

    Add csv jar

--- BitMapPro/src/ReadData.java
+++ BitMapPro/src/ReadData.java
@@ -1,8 +1,10 @@
+import au.com.bytecode.opencsv.CSVReader;
+
 
 public class ReadData {
 	
    public ReadData(String path){
-	   
+	   CSVReader csvReader = new CSVReader(null);
    }
    public void read(){
 		
